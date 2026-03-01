576a7b0017776e6767a2841bec6788c1a1c785cd
Eddys912 <eddys912290702@gmail.com>
2026-02-28 19:37:53 -0600

    feat: add icons

---
+++ web/src/icons/ui/Search.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let { class: className, ...props } = $props();
+</script>
+
+<svg
+  class={className}
+  {...props}
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round">
+  <path d="m21 21-4.34-4.34"></path>
+  <circle cx="11" cy="11" r="8"></circle>
+</svg>
