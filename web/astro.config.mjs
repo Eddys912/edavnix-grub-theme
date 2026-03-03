0aebd9642fc81b66e2c8217962959c1d5011a5e4
Eddys912 <eddys912290702@gmail.com>
2026-03-02 22:02:45 -0600

    feat: add environment variables

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,13 +4,57 @@ import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  integrations: [svelte()],
   site: import.meta.env.SITE,
   env: {
     schema: {
-      SITE: envField.string({ context: 'server', access: 'public', optional: true }),
+      SITE: envField.string({
+        context: 'server',
+        access: 'public',
+      }),
+      SITE_TITLE: envField.string({
+        context: 'server',
+        access: 'public',
+        default: 'Edavnix - Custom themes for GRUB inspired by Linux distributions',
+      }),
+      SITE_DESC: envField.string({
+        context: 'server',
+        access: 'public',
+        default:
+          'Customize your PC startup with the best themes for GRUB and Ventoy. Download exclusive skins inspired by your favorite Linux distributions and give your startup menu a professional and aesthetic look.',
+      }),
+      SITE_KEYWORDS: envField.string({
+        context: 'server',
+        access: 'public',
+        default: 'edavnix, linux, themes, grub, ventoy',
+      }),
+      SITE_OG_IMAGE: envField.string({
+        context: 'server',
+        access: 'public',
+        default: '/og.png',
+      }),
+      SITE_AUTHOR: envField.string({
+        context: 'server',
+        access: 'public',
+        default: 'edavnix',
+      }),
+      SITE_THEME_COLOR: envField.string({
+        context: 'server',
+        access: 'public',
+        default: '#0f0f0f',
+      }),
+      SITE_NAME: envField.string({
+        context: 'server',
+        access: 'public',
+        default: 'Edavnix',
+      }),
+      SITE_LOCALE: envField.string({
+        context: 'server',
+        access: 'public',
+        default: 'en_US',
+      }),
     },
   },
-  integrations: [svelte()],
   image: {
     service: sharpImageService(),
     experimentalLayout: 'responsive',
