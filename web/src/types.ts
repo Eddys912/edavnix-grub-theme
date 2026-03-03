92e861ff9a26d6c9aa30641876c65626f3299a9a
Eddys912 <eddys912290702@gmail.com>
2026-03-02 18:03:32 -0600

    fix: update types

--- web/src/types.ts
+++ web/src/types.ts
@@ -5,7 +5,7 @@ export type ThemeSlug = CollectionEntry<'distros'>['slug'];
 
 export type AllData = ThemeData & { slug: ThemeSlug };
 
-export type Paint = Pick<AllData, 'slug' | 'name' | 'suffix' | 'color' | 'icon'>;
+export type Paint = Pick<ThemeData, 'name' | 'suffix' | 'color' | 'icon'>;
 
 export type Card = Pick<
   AllData,
